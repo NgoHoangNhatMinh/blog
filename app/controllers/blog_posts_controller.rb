57b15bb6d490bbf073990bb205e815573bdb3607
Minh <ngohoangnhatminh@gmail.com>
2024-12-20 16:22:58 +0800

    Add update,  destroy function for posts

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,10 +1,10 @@
 class BlogPostsController < ApplicationController
+  before_action :set_post, only: %i[ show edit update destroy]
   def index
     @posts = BlogPost.all
   end
 
   def show
-    @post = BlogPost.find(params[:id])
   end
 
   def new
@@ -20,7 +20,27 @@ class BlogPostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to blog_posts_path
+  end
+
   private
+    def set_post
+      @post = BlogPost.find(params[:id])
+    end
+
     def post_params
       params.expect(blog_post: [ :title, :body ])
     end
